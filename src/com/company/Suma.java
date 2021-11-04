3a33e64ccd9048e0764584b514afb3119aee3b83
Jakub Pánek <43809342+Soxori@users.noreply.github.com>
2021-11-04 17:14:53 +0100

    first commit

---
+++ src/com/company/Suma.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Suma {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = 0;
+        int y = 0;
+
+        System.out.println("Enter number");
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        while(sc.hasNext()) {
+            list.add(sc.nextInt());
+        }
+
+        for (Integer integer : list) {
+            x += integer;
+        }
+
+        double idk = (double) x / list.size();
+
+        for (Integer integer : list) {
+            if (integer >= idk) {
+                y = y + integer;
+            }
+        }
+        System.out.println(y);
+    }
+}
